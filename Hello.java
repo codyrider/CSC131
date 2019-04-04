6040141e3788715fd9acc8468ec713d96f838f58
zero3 <zero3@LAPTOP-L9MGOIV4>
2019-04-03 19:28:05 -0700

    Pushing change to Hello.java to create error

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219199618 - This line was added by Cody Rider.");
-		System.out.println("219461048 - This line was added by Lucas Sheehy.");
+		System.out.println("219461048 - This line was added by  Lucas Sheehy.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
