af6ef9020c613d069d11cb6ad20cd06895e1b794
zxjon <zxjon@DESKTOP-2G1F7QM>
2019-04-04 19:40:07 -0700

    Merge branch 'master' of https://github.com/codyrider/CSC131

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
-		// Add a new line with your information
+		 // Add a new line with your information
 		System.out.println("219199618 - This line was added by Cody Rider.");
 		System.out.println("219461048 - This line was added by  Lucas Sheehy.");
 		System.out.println("217379124 - This line was added by Jamie Kim. ");
