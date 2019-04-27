5922a24c9ca6dbbc873b856f5237cd1a3e373658
codyrider <33608379+codyrider@users.noreply.github.com>
2019-04-26 22:07:00 -0700

    fleshed out the user class. Not sure if we need both userID & userName. Also, will the user need to call anything such as device? Will user have a tag class that will hold the id?

---
+++ 131/src/org/csus/csc131/user.java
@@ -0,0 +1,54 @@
+/*
+ * User class for storing user information
+ */
+
+public class user {
+	
+	private enum status {LOST, FOUND};
+	private int userID;
+	private int tagID;
+	private status tagStatus;
+	private String userName; //do we need this and userID??
+
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public int getTagID() {
+		return tagID;
+	}
+	
+	public status getTagStatus() {
+		return tagStatus;
+	}
+	
+	public String UserName() {
+		return userName;
+	}
+	
+	public void setLost() {
+		tagStatus = status.LOST;
+	}	
+	
+	public void setFound() {
+		tagStatus = status.FOUND;
+	}
+	
+	public void setTagID(int tag) {
+		tagID = tag;
+	}
+	
+	public void setUserName(String name) { //need both username and userID?
+		userName = name;
+	}
+	
+	public void setUserID(int id) {
+		userID = id;
+	}
+	
+	public void setUserName() {
+		
+	}
+	
+}
