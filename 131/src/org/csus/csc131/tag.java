f42d6ac0f388edcbcd0e54d46afac107579871c9
403850323 <zxjonathanzx@gmail.com>
2019-05-06 19:24:59 -0700

    Update tag.java

--- 131/src/org/csus/csc131/tag.java
+++ 131/src/org/csus/csc131/tag.java
@@ -1,12 +1,16 @@
 package org.csus.csc131;
 
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class tag 
 {
 	private int tagID;
 	
-	public tag()
-	{
-		tagID = 111;
+	public tag() throws IOException{
+		tagID = createID();
+		increaseID();
 	}
 	
 	public int getTagID()
@@ -14,9 +18,23 @@ public class tag
 		return tagID;
 	}
 	
-	//private int genTagID()
-	//{
-	//	tagID = 123;
-	//	return tagID;
-	//}
+	private int createID()throws IOException{
+		int a=0;
+      
+		Scanner scan=new Scanner(new File("tagID.txt"));
+			
+		a=scan.nextInt();
+			
+		return a;
+	}
+	
+	private void increaseID() throws IOException{
+		
+		
+		FileWriter writer = new FileWriter("tagID.txt");
+			int a=tagID+1;
+			writer.write(String.valueOf(a));
+			writer.close();
+		}
+	
 }
