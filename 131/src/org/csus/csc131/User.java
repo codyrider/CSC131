54d611df2617fb3f54d4f41b2dc703b32a8b5a5b
codyrider <33608379+codyrider@users.noreply.github.com>
2019-05-06 19:52:44 -0700

    delete unused method and added comments

--- 131/src/org/csus/csc131/User.java
+++ 131/src/org/csus/csc131/User.java
@@ -9,7 +9,7 @@ public class User {
 	private int tagID;
 	private String tagStatus;
 
-	
+//constructors	
 	public User(int userID) {
 		this.userID = userID;
 	}
@@ -20,36 +20,40 @@ public class User {
 		this.tagStatus = tagStatus;
 	}
 
+	
+//retrieve UserID
 	public int getUserID() {
 		return userID;
 	}
-	
+
+//retrieve tagID
 	public int getTagID() {
 		return tagID;
 	}
 	
+//retrieve status of tag; lost or found
 	public String getTagStatus() {
 		return tagStatus;
 	}
-	
+
+//set status of tag to lost
 	public void setLost() {
 		tagStatus = "lost";
 	}	
-	
+
+//set status of tag to found
 	public void setFound() {
 		tagStatus = "found";
 	}
-	
+
+//assign tag to user
 	public void setTagID(int tag) {
 		tagID = tag;
 	}
-	
+
+//assign userID to user
 	public void setUserID(int id) {
 		userID = id;
-	}
-	
-	public void setUserName() {
-		
 	}
 	
 }
