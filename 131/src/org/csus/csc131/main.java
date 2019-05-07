469d74d391a6323df0e76d53938a4c9e062d2f12
James <James@192.168.7.135>
2019-05-06 21:22:50 -0700

    new Main

--- 131/src/org/csus/csc131/main.java
+++ 131/src/org/csus/csc131/main.java
@@ -7,7 +7,9 @@ import java.lang.*;
 public class main {
 	
 	static Scanner input = new Scanner(System.in); //global variable for getting input
-	static server ser = new server(); //global server object;
+	static device dev = new device();
+	static int tagID;
+	
 	
 	public static void main(String[] args) {
 				
@@ -19,21 +21,21 @@ public class main {
 		
 	public static void menu1() {
 		System.out.println("\n\n----------------------------------------------------------------------------------------------------\n");
-		System.out.println("(1) New User (2) Existing User (3) Quit \n");
+		System.out.println("(1) New User (2) Existing User (3)Run from tag (4) Quit \n");
 		System.out.println("Select out of the two options: ");
 		int menu1 = input.nextInt();
 		switch(menu1) {
 		case 1:
 			System.out.println("Create unique User ID: ");
 			int newUser = input.nextInt();
-			ser.addUser(newUser);
+			dev.createUser(newUser);
 			System.out.println("UserId Successfully Created! \n\n");
 			menu2(newUser);
 			break;
 		case 2:
 			System.out.println("Enter in your unique User ID: ");
 			int existingUser = input.nextInt();
-			while(ser.userExists(existingUser) != true) {
+			while(dev.logIn(existingUser) != true) {
 				System.out.println("User ID Not Found - Try Again (0 to quit): ");
 				existingUser = input.nextInt();
 				if(existingUser == 0) {
@@ -47,6 +49,9 @@ public class main {
 			menu2(existingUser);
 			break;
 		case 3:
+			dev.tagFound(tagID);
+			break;
+		case 4:
 			System.out.println("Goodbye! \n");
 			break;
 		default:
@@ -66,13 +71,13 @@ public class main {
 		switch(menu2) {
 		case 1:
 			System.out.println("Please enter in your item ID: ");
-			int tagID = input.nextInt();
-			device newdev = new device(uid); //create a device object passing in uid to link object to the user
-			newdev.addTag(tagID); //add the device via itemID into database linking it to it's owner
+			tagID = input.nextInt();
+			//device newdev = new device(uid); //create a device object passing in uid to link object to the user
+			dev.addTag(tagID); //add the device via itemID into database linking it to it's owner
 			System.out.println("Device successfully added \n");
 			//menu2(uid);
 			break;
-		case 2:
+		/*case 2:
 			System.out.println("Please enter in item ID that is found: ");
 			int foundTagID = input.nextInt();
 			device foundDev = new device();
@@ -81,18 +86,18 @@ public class main {
 			ser.tagFound(foundTagID,gps);
 			System.out.println("Item "+foundTagID+" has been set to found!\n");
 			menu2(uid);
-			break;
+			break;*/
 		case 3:
 			System.out.println("Please enter in tag ID of your lost item: ");
 			int lostTagID = input.nextInt();
-			device lostDev = new device();
-			lostDev.setLost(lostTagID);
+			//device lostDev = new device();
+			dev.setLost(lostTagID);
 			System.out.println("Item "+lostTagID+" has been set to lost.\n");
 			menu2(uid);
 			break;
 		case 4:
 			//User user = new User(uid);
-			System.out.println("Status: "+ser.checkTagStatus(uid));
+			System.out.println("Status: "+dev.checkStatus(uid,tagID));
 			menu2(uid);
 			break;
 		case 5:
