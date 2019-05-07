d90af393a46541a528e5ffe1976a3c13b72aaf5d
403850323 <zxjonathanzx@gmail.com>
2019-05-06 19:33:21 -0700

    Update tag.java

--- 131/src/org/csus/csc131/tag.java
+++ 131/src/org/csus/csc131/tag.java
@@ -4,36 +4,56 @@ import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class tag 
-{
+/**
+ * A class create Tag ID
+ * @author Hung Ming, Liang
+ * @version 6 May 2019
+ */
+
+public class tag {
+
 	private int tagID;
 	
+	/**
+		* Initializes tag class
+		* get number from file and set as tagID
+		* update tagID in txt file
+	*/
 	public tag() throws IOException{
 		tagID = createID();
 		increaseID();
 	}
 	
+	/**
+		* return tagID;
+	*/
 	public int getTagID()
 	{
 		return tagID;
 	}
 	
+	/**
+		* Read txt File and set new tag ID;
+	*/
 	private int createID()throws IOException{
 		int a=0;
       
-		Scanner scan=new Scanner(new File("tagID.txt"));
+		Scanner scan=new Scanner(new File("tagID.txt"));//read tagID.txt for tagID
 			
 		a=scan.nextInt();
 			
 		return a;
 	}
 	
+	/**
+		* write txt file and update id number;
+	*/
 	private void increaseID() throws IOException{
 		
 		
 		FileWriter writer = new FileWriter("tagID.txt");
 			int a=tagID+1;
-			writer.write(String.valueOf(a));
+			writer.write(String.valueOf(a));//write new number to tagID.txt
 			writer.close();
 		}
 	
